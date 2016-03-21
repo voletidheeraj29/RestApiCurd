2312b953698f20bff32927555ca53971851a7666
unknown <dheerajv@dheerajv.cybage.com>
2016-03-21 11:16:21 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 var express = require('express');
-var app = express();
+var logger = require('morgan');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var app = express();
 var route = require('./rest');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended : false
-}));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', route);
+
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use(function(err, req, res, next) {
+	res.status(400);
+	var data = {
+			"status":400,
+			"description":"Invalid JSON structure",		
+			"data":null,
+			"error":[{"msg":"Unparsable JSON format"}]	
+	};
+	res.json(data);
+});
 
 module.exports = app;
--- rest.js
+++ rest.js
@@ -9,182 +9,243 @@ var connection = mysql.createConnection({
 	database : 'dheeraj',
 });
 
-var users = {
-		"status":1,
-		"user":""
+var data = {
+		"status":0,
+		"description":null,		
+		"data":null,
+		"error":[{"msg":null}]	
 };
 
+//Alphabetical String Checking
+function allLetter(inputtxt)  
+{  
+	var letters = /^[A-Za-z]+$/; 
+	var mail =/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/;
+	
+	if(letters.test(inputtxt) || mail.test(inputtxt))  
+	{  
+		return true;  
+	}  
+	else  
+	{  		
+		return false;  
+	}  
+}  
+
+//GET ALL USERS
 route.get('/users', function(req, res) {
 
 	connection.query("select * from user1", function(err, rows, fields) {
-		if (rows.length !== 0) {
-			users.status = 200;
-			users.user = rows;
-			res.json(users);
-		} else {
-			users.status = 200;
-			users.user = "No User found....";
-			res.json(users);
+		if (err) {
+			data.status = 500;			
+			data.description="Error ocuured : " + err.code;
+			data.data=null;
+			data.error[0].msg=err;		
 		}
+		else if (rows.length !== 0) {			
+			data.status = 200;			
+			data.description="Returning all Users";
+			data.data=rows;							
+		} 
+		else {
+			data.status = 200;			
+			data.description="No User exists into database.";
+			data.data=null;
+			data.error[0].msg=null;
+		}
+		res.json(data);
 	});
 });
 
+//GET USER BY ID
 route.get('/users/:id', function(req, res) {
 
-	console.log(isNaN(req.params.id));
 	if(isNaN(req.params.id)){
-		users.status = 400;
-		users.user = "Invalid URL request";
-		res.json(users);
 
+		res.status(400);
+		data.status = 400;			
+		data.description="Bad Request";
+		data.error[0].msg="Invalid service URI";
+		res.json(data);
 	}else {
 		var id= parseInt(req.params.id);
 
 		connection.query("select * from user1 where id=?",[id], function(err, rows, fields) {
-			if (rows.length !== 0) {
-				users.status = 200;
-				users.user = rows;
-				res.json(users);
-			}else { 
-				res.status(404);
-				users.status = 404;
-				users.user = "No user found with requested id....";
-				res.json(users);
-			}	 
+			if (err) {
+				res.status(500);
+				data.status = 500;			
+				data.description="Error ocuured : " + err.code;
+				data.error[0].msg=err;							
+			}
+			else if (rows.length !== 0) {			
+				data.status = 200;			
+				data.description="Returning requested user with id = "+id;
+				data.data=rows;					
+				data.error[0].msg=null;
+			} 
+			else {
+				data.status = 200;			
+				data.description="No user found with requested id";	
+				data.data=null;
+				data.error[0].msg=null;
+			}	
+			res.json(data);
 		});
 	}
 
 });
 
+//INSERT USER INTO DATABASE
 route.post('/users',function(req,res){
+
 	var Firstname = req.body.firstname;
 	var Lastname = req.body.lastname;
 	var Email = req.body.email;
-	var id = parseInt(req.body.id);
+	var id=null;
 
-	if(!!Firstname && !!Lastname && !!Email && !!id){
+	if(!!Firstname && !!Lastname && !!Email){
 
-		if(typeof Firstname === 'string' && typeof Lastname === 'string' && typeof Email === 'string' && typeof id === 'number'){
+		if(allLetter(Firstname) && allLetter(Lastname) && allLetter(Email)){
 
 
 			connection.query("INSERT INTO user1 VALUES(?,?,?,?)",[Firstname,Lastname,Email,id],function(err, rows, fields){
 				if(!!err){  
-					// console.error('There was an error',err);
 					res.status(500);
-					users.status = 500;
-					users.user = "Internal Server Problem";
+					data.status = 500;			
+					data.description="Error ocuured : " + err.code;
+					data.data=null;					
+					data.error[0].msg=err;			
 
 				}else{
 					res.status(201);
-					users.status = 201;
-					users.user = "User inserted into database";
+					data.status = 201;			
+					data.description="User inserted into database";
+					data.data=null;								
+					data.error[0].msg=null;
 				} 
-				res.json(users);	
-
+				res.json(data);	
 			});
 		}else{
-			res.status(400);
-			users.status = 400;
-			users.user = "Incorrect Json Structure";
-			res.json(users);
+			res.status(200);
+			data.status = 200;			
+			data.description="Firstname, Lastname or email are not correct";
+			data.data=null;			
+			data.error[0].msg="Invalid firstname or lastname or email";
+			res.json(data);
 		}
 	}else{
 		res.status(422);
-		users.status = 422;
-		users.user = "Missing data in Json....";
-		res.json(users);
+		data.status = 422;			
+		data.description="Missing data in JSON";
+		data.data=null;		
+		data.error[0].msg="Missing a required param in Json. Please check your JSON request";
+		res.json(data);	
 	}
 
 
 });
 
+//UPDATE USER
 route.put('/users',function(req,res){ 
 
 	var Firstname = req.body.firstname;
 	var Lastname = req.body.lastname;
 	var Email = req.body.email;
 	var id = parseInt(req.body.id);
-	connection.query("select * from user1 where id=?",[id],function (err, rows, fields) {    	
-		if (rows.length == 0) {
-			res.status(404);
-			users.status = 404;
-			users.user = "No data found...";
-			res.json(users);
 
-		}
-		else{
-			if(!!Firstname && !!Lastname && !!Email && !!id){    	    	
-				if(typeof Firstname === 'string' && typeof Lastname === 'string' && typeof Email === 'string' && typeof id === 'number'){   	    		
-					connection.query("UPDATE user1 SET ?,?,? WHERE ?",[{firstname: Firstname},{lastname: Lastname},{email: Email},{id: id}],function(err, rows, fields){
-						if(err){
-							res.status(500);
-							users.status = 500;
-							users.user = "Internal error occured...";
-							res.json(users);
-						}else{            	
-							users.status = 200;
-							users.user = "User updated into database";
-							res.json(users);
-						}
-
-					});
-
-				}else{
-					res.status(400);
-					users.status = 400;
-					users.user = "Incorrect Json Structure";
-					res.json(users);
+	if(!!Firstname && !!Lastname && !!Email && !!id){    	    	
+		if(allLetter(Firstname) && allLetter(Lastname) && allLetter(Email) && typeof id === 'number'){   	    		
+			connection.query("UPDATE user1 SET ?,?,? WHERE ?",[{firstname: Firstname},{lastname: Lastname},{email: Email},{id: id}],function(err, rows, fields){
+				if(err){
+					res.status(500);
+					data.status = 500;			
+					data.description="Error ocuured : " + err.code;
+					data.data=null;							
+					data.error[0].msg=err;								
 				}
-			}else{
-				res.status(422);
-				users.status = 422;
-				users.user = "Missing data in json";
-				res.json(users);
-			}
+				else if (rows.affectedRows == 0) {
+					res.status(200);
+					data.status = 200;			
+					data.description="No user exists with id = " + id;	
+					data.data=null;
+					data.error[0].msg=null;
+				}
+				else{            	
+					res.status(200);
+					data.status = 200;			
+					data.description="User updated into database";
+					data.data=null;								
+					data.error[0].msg=null;
+				}
+				res.json(data);
+			});
+
+		}else{
+			res.status(200);
+			data.status = 200;			
+			data.description="Firstname, Lastname or email are not correct";
+			data.data=null;			
+			data.error[0].msg="Invalid firstname or lastname or email";
+			res.json(data);
 		}
-	});
-});
-
-route.delete('/users/:id',function(req,res){
-
-	if(isNaN(req.params.id)){
-		users.status = 400;
-		users.user = "Invalid URL request";
-		res.json(users);
-
-	}else {
-
-		var id = parseInt(req.params.id);
-		connection.query("select * from user1 where id=?",[id],function (err, rows, fields) {    	
-			if (rows.length == 0) {
-				res.status(404);
-				users.status = 404;
-				users.user = "No data found...";
-				res.json(users);
-
-			}else{
-
-				connection.query("DELETE FROM user1 WHERE id=?",[id],function(err, rows, fields){
-					if(err){   
-						res.status(404);
-						users.status = 404;
-						users.user = "No user found...";
-						res.json(users);
-					}else{           
-						users.status = 200;
-						users.user = "User deleted successfully";
-						res.json(users);
-					}           
-				});
-			}
-		});
+	}else{
+		res.status(422);
+		data.status = 422;			
+		data.description="Missing data in JSON";
+		data.data=null;				
+		data.error[0].msg="Missing a required param in Json. Please check your JSON request";
+		res.json(data);
 	}
 });
 
+//DELETE USER BY ID
+route.delete('/users/:id',function(req,res){
+
+	if(isNaN(req.params.id)){
+		res.status(400);
+		data.status = 400;			
+		data.description="type of requested id is not a number";
+		data.data=null;		
+		data.error[0].msg="Bad Request";			
+		res.json(data);
+
+	}else {
+
+		var id = parseInt(req.params.id);		
+		connection.query("DELETE FROM user1 WHERE id=?",[id],function(err, rows, fields){
+			if(err){   
+				res.status(500);
+				data.status = 500;			
+				data.description="Error ocuured : " + err.code;
+				data.data=null;				
+				data.error[0].msg=err;
+			}
+			else if (rows.affectedRows == 0) {
+				res.status(200);
+				data.status = 200;			
+				data.description="No user exists with id = " + id;	
+				data.data=null;
+				data.error[0].msg=null;
+			}
+			else{           
+				data.status = 200;			
+				data.description="User deleted with id = "+id;	
+				data.data=null;
+				data.error[0].msg=null;
+			}        
+			res.json(data);
+		});			
+	}	
+});
+
+//INVALID URI REQUEST
 route.all('*', function(req, res) {
-	users.status = 400;
-	users.user = "Invalid URL request";
-	res.json(users);
+	res.status(400);
+	data.status = 400;			
+	data.description="Bad Request";
+	data.data=null;	
+	data.error[0].msg="Invalid service URI";
+	res.json(data);
 });
 
 module.exports = route;
